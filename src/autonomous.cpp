b1422775eb0d24b95c6a627693866f1b793487a3
Nik Dunna <nik.dunna@gmail.com>
2019-01-15 18:56:15 -0500

    Auton work

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,20 +2,39 @@
 
 void redAuton()
 {
- in.move(127);
    while(!cataLimit.get_value())
    {
      cata.move(-127);
    }
    cata.move(-10);
+    in.move(127);
    left.tarePosition();
    right.tarePosition();
   left.moveAbsolute(-1200, 200);
   right.moveAbsolute(-1200, 200);
   pros::delay(1600);
-  left.moveRelative(975, 200);
-  right.moveRelative(975, 200);
-  pros::delay(700);
+  left.moveVoltage(9000);
+  right.moveVoltage(9000);
+  pros::delay(2000);
+  left.moveVoltage(0);
+  right.moveVoltage(0);
+  pros::delay(300);
+  left.moveRelative(-300, 200);
+  right.moveRelative(-300, 200);
+  pros::delay(1000);
+  left.moveRelative(276, 200);
+  right.moveRelative(-276, 200);
+  pros::delay(600);
+  cata.move(-127);
+  pros::delay(300);
+  cata.move(0);
+  pros::delay(200);
+  left.moveVoltage(9000);
+  right.moveVoltage(9000);
+  pros::delay(2000);
+  left.moveVoltage(0);
+  right.moveVoltage(0);
+
 }
 
 void blueAuton()
