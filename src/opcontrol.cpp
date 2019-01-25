b6dbc0c5dacffe639e54bce0736c6d65eebedda6
attavex <nik.dunna@gmail.com>
2019-01-24 23:55:29 -0500

    Okapi tested and confirmed to work in opcontrol with macro... testing auton in the future. Tab view added for LVGL control in opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,7 +1,23 @@
 #include "main.h"
-
+static lv_obj_t * background;
 void opcontrol()
 {
+  background = lv_page_create(NULL, NULL);
+  lv_scr_load(background);
+
+   /* Creates Tab View */
+    lv_obj_t * tabViewOne = lv_tabview_create(background, NULL);
+    lv_tabview_add_tab(tabViewOne, "Motor Values");
+    lv_tabview_add_tab(tabViewOne, "Sensor Values");
+	lv_obj_set_size(tabViewOne, LV_HOR_RES, LV_VER_RES);
+
+	//lv_obj_t * 
+
+
+
+
+
+
 	
 	//CATA//
 	//pros::Task activityCata(cataActivity, NULL);
@@ -28,6 +44,17 @@ void opcontrol()
         else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) cata.move(127);
 		else cata.move(13);
 		*/
+
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+		{
+	profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{3_ft, 0_ft, 0_deg}}, "Test");
+      profileController.setTarget("Test");
+      profileController.waitUntilSettled();
+		}
+
+
+
+
 		pros::delay(20);
 	}
 }
